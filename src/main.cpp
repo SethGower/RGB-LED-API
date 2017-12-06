ff553315ec276b053d03a8c761f3baa835278da0
Seth Gower <sethzerish@gmail.com>
2017-12-06 14:04:59 -0500

    adds pink

--- src/main.cpp
+++ src/main.cpp
@@ -28,8 +28,8 @@ String readString = String(100);
 String finalString = String(100);
 
 // Color Definitions
-String colorNames[] = {"white", "red", "yellow", "green", "aqua", "blue", "purple", "csh"};
-String colorValues[] = {"0xFFFFFF", "0xFF0000", "0xFFFF00", "0x00FF00", "0x00FFFF", "0x0000FF", "0x800080", "0xC1007C"};
+String colorNames[] = {"white", "red", "yellow", "green", "aqua", "blue", "purple", "pink", "csh"};
+String colorValues[] = {"0xFFFFFF", "0xFF0000", "0xFFFF00", "0x00FF00", "0x00FFFF", "0x0000FF", "0x800080", "0xFF69B4", "0xC1007C"};
 // Color Definitions
 
 /*
